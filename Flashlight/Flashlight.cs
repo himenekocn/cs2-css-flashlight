6ee71c50b0e475853981a2c65650d6a758aa1c7c
LynchMus <xubw@vip.qq.com>
2024-02-24 00:25:10 +0800

    Update Flashlight.cs

--- Flashlight/Flashlight.cs
+++ Flashlight/Flashlight.cs
@@ -13,7 +13,7 @@ public class Flashlight : BasePlugin
 {
     public override string ModuleAuthor => "creazy.eth";
     public override string ModuleName => "Flashlight";
-    public override string ModuleDescription => "Flashlight for Counter-Strike 2";
+    public override string ModuleDescription => "Flashlight for Counter-Strike 2 ";
     public override string ModuleVersion => "0.0.6";
 
     private static string ModuleDisplayName => "Flashlight";
